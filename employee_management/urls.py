da220737fd1dfc39333834dd9e3c81154111537f
Pravesh-Sudha <88569706+Pravesh-Sudha@users.noreply.github.com>
2025-08-07 13:08:07 +0530

    Update urls.py

--- employee_management/urls.py
+++ employee_management/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+def health(request):
+    return HttpResponse("OK", status=200)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('employees.urls')),  # Include the employees app routes
+    path('health/', health, name='health'),
 ]
