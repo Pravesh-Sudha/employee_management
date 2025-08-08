c570325939e1d43f21635751848987543bf2f804
Pravesh-Sudha <88569706+Pravesh-Sudha@users.noreply.github.com>
2025-08-08 13:39:07 +0530

    Update urls.py

--- employee_management/urls.py
+++ employee_management/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 def health(request):
     return HttpResponse("OK", status=200)
@@ -7,5 +8,6 @@ def health(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('employees.urls')),  # Include the employees app routes
-    path('health/', health, name='health'),
+    path('health', health, name='health'),  # No trailing slash for ALB
+    path('health/', health, name='health_slash'),  # With trailing slash
 ]
